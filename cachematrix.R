0cff99a53abf88823c9b74baa3a410adacc62a79
josiahnormand <josiah_n@yahoo.com>
2015-07-26 13:21:46 -0500

    Checked in makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below work by creating a square invertible matrix
+## and storing the inverse of the matrix in cache
 
-## Write a short comment describing this function
+
+
+
+
+## makeCacheMatrix builds and returns a list of functions
+## in order for cacheSolve to retrive or store 
+## the inverted matrix in cache
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        # set localcache to NULL
+        localcache <- NULL
+  
+        # build matrix in working environment
+        store <- function(z) {
+                x <<- z
+                localcache <<- NULL
+        }
+  
+        # retrive value of the matrix
+        retrive <- function() x
+        # invert the matrix and store in localcache
+        storeMatrix <- function(inverse) localcache <<- inverse
+        # retrive inverted matrix from localcache
+        retriveInverse <- function() localcache
+  
+        # return functions
+        list(store = store, retrive = retrive,
+             storeMatrix = storeMatrix,
+             retriveInverse = retriveInverse)
 }
 
 
